4cfa698b8bffd5dab99e83b2684663fbb7c6bdb6
espressocalculator <tobi_667@web.de>
2025-07-25 11:56:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RecipePage from "./pages/RecipePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Layout from "./components/Layout";
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:slug" element={<RecipePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
+  );
+}
 
